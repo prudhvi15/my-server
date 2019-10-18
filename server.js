fa4791a4f5645086d356fe8d421f46617956522b
venkat prudhvi Dommaraju <S537236@nwmissouri.edu>
2019-10-18 01:13:38 -0500

    updated server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain')
   res.write('Hello...\n')
   res.write('Hello...\n')
-  res.end('Hello World! This is text - we can respond with HTML, JSON, and more :)\n')
+  res.end('Hello World! This is prudhvi server - we can respond with HTML, JSON, and more :)\n')
 })
 
 // start listening
